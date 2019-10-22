245c6b9be42228e4183e14420405ef0363c01628
divya <arkr1089@gmail.com>
2019-10-22 14:04:54 -0400

    this is commit from IST

---
+++ src/test/java/com/maven/SeleniumTest.java
@@ -0,0 +1,14 @@
+package com.maven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void test() {
+		System.out.println("This is Selenium Test");
+	}
+
+}
