b77fd985730b0b457b268f42a8cbd37d49123f38
divya <arkr1089@gmail.com>
2019-10-22 14:23:41 -0400

    this is commit from EST

---
+++ src/test/java/com/maven/AppiumTest.java
@@ -0,0 +1,14 @@
+package com.maven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void test() {
+		System.out.println("This Appium Test");
+	}
+
+}
